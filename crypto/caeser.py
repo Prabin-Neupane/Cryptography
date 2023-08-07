6ea1e70ee38e1817f3730d2b47c5f59589dde8c1
Prabin-Neupane <neupaneprabin40@gmail.com>
2023-08-07 17:05:58 +0545

    yes

--- crypto/caeser.py
+++ crypto/caeser.py
@@ -1,2 +1,13 @@
-print("hello")
-print("hi")
+def enc(plain,key):
+    result=""
+    for i in range(len(plain)):
+        char = plain[i]
+        if(char.isupper()):
+            result+= chr((ord(char)-65+key)%26+65)
+        else:
+            result+= chr((ord(char)+key-97)%26+97)
+    return result
+
+text=input("enter plain text : ")
+key=int(input("enter a key : "))
+print("cipher text is : " + enc(text,key))
